4b1b323d9303b28400c03ccb607f0c8ea97f9fce
Elaine <elainedoud@gmail.com>
2023-07-31 13:42:30 -0400

    Added some database data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+bands = Band.create({name: "Spice Girls"}, {name: "NSYNC"}, {name: "Backstreet Boys"})
