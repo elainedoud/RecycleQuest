5b8800e5d743a2cb15a447b3a2ba5f02d94a2c1e
Sofia Katzman <katzmansof@gmail.com>
2023-08-10 21:42:24 -0400

    restructure

--- client/src/components/Pages/Leaderboard/Leaderboard.js
+++ client/src/components/Pages/Leaderboard/Leaderboard.js
@@ -3,7 +3,7 @@ import React from "react"
 function Leaderboard(){
     return(
         <>
-        <h4>This is Leaderboard</h4>
+        <h4>LEADERBOARD</h4>
         <p>Compare all users to our logged in user and show their ranking</p>
         <p>Suggest how they can earn more points - links to submit a recycling plant or redeem recyclables</p>
         </>
