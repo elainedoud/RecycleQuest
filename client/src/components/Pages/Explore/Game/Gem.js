d4debc85ff0f72a33d72863e7f6af9a04d94a8b3
Sofia Katzman <katzmansof@gmail.com>
2023-08-15 23:46:34 -0400

    add gem component to display gems on map

---
+++ client/src/components/Pages/Explore/Game/Gem.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './Gem.css';
+
+function Gem({ gem, onClick, hidden }) {
+  const handleClick = () => {
+    onClick(gem);
+  };
+
+  return (
+    <div
+      className={`gem ${hidden ? 'hidden' : ''}`}
+      style={{ top: `${gem.top}%`, left: `${gem.left}%` }}
+      onClick={handleClick}
+    >   
+        {/* div below gem to glow since gem is not a square object */}
+        <div className="glow"/> 
+        <img src="https://freepngimg.com/save/56561-gem-download-hq-png/554x361" className="gem-image" alt="gem" />
+      
+    </div>
+  );
+}
+
+export default Gem;
