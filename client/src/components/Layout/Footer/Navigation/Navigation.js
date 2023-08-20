a5b624288e4ad56e70658e5961609adbcace244a
Sofia Katzman <katzmansof@gmail.com>
2023-08-20 14:34:41 -0400

    remove pan&zoom for info popups

--- client/src/components/Layout/Footer/Navigation/Navigation.js
+++ client/src/components/Layout/Footer/Navigation/Navigation.js
@@ -6,7 +6,7 @@ function Navigation(){
     return(
         <div>
         <ul>
-            <li><Link to="/">login</Link><br/></li>
+            {/* <li><Link to="/">login</Link><br/></li> */}
             <li><Link to="/home">home</Link><br/></li>
             <li><Link to="/explore">explore</Link><br/></li>
             <li><Link to="/locate">locate</Link><br/></li>
