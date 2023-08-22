929e37e75b620cc34ed078d1954440ac2264f55a
Sofia Katzman <katzmansof@gmail.com>
2023-08-21 21:20:30 -0400

    update userContext to account for points, and eventTimes

--- client/src/components/Context/UserContext.js
+++ client/src/components/Context/UserContext.js
@@ -4,7 +4,11 @@ const UserContext = createContext()
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null)
-
+  const [userPoints, setUserPoints] = useState(null)
+  const [lastDailyBonus, setLastDailyBonus] = useState(null)
+  // const [lastDailyQuiz, setLastDailyQuiz] = useState(null)
+  const [lastDailyGems, setLastDailyGems] = useState(null)
+  
   useEffect(() => {
     fetch("/user")
       .then((res) => {
@@ -16,13 +20,25 @@ export function UserProvider({ children }) {
       })
       .then((data) => {
         setUser(data)
-        console.log(data) // Set the fetched user data
+        setUserPoints(data.total_points_count)
+        // console.log(data) // Set the fetched user data
       })
       .catch((error) => {
         console.error("Error fetching user:", error)
         setUser(null)
       })
   }, [])
+
+  useEffect(()=> {
+    if(user){
+      setLastDailyBonus(user.last_daily_bonus)
+      // setLastDailyQuiz(user.last_daily_quiz)
+      setLastDailyGems(user.last_daily_gems)
+    }
+    else{
+      console.log("no user data yet.")
+    }
+  }, [user])
 
   // Function to update the user data
   const updateUser = (userData) => {
@@ -31,7 +47,7 @@ export function UserProvider({ children }) {
 
   // Return the UserContext Provider with the user and updateUser in value
   return (
-    <UserContext.Provider value={{ user, updateUser }}>
+    <UserContext.Provider value={{ user, updateUser, userPoints, setUserPoints }}>
       {children}
     </UserContext.Provider>
   )
