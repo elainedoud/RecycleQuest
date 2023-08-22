5f7f27cae172f429df943e3db0d86412669e1f1c
Elaine <elainedoud@gmail.com>
2023-08-22 08:27:21 -0400

    Fixed small syntax error on question serializer

--- app/serializers/questions_serializer.rb
+++ app/serializers/questions_serializer.rb
@@ -1,5 +1,5 @@
 class QuestionsSerializer < ActiveModel::Serializer
-  attributes :id, :query, :A, :B, :C :answer
+  attributes :id, :query, :A, :B, :C, :answer
 
   has_many :points 
   has_many :users, through: :points
