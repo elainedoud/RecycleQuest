492c36dbdcc4dd0f5d1c6498d19a3c8142f004a8
Sofia Katzman <katzmansof@gmail.com>
2023-08-22 14:09:59 -0400

    add gem count

--- client/src/components/Pages/Explore/Explore.js
+++ client/src/components/Pages/Explore/Explore.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MapInteractionCSS } from 'react-map-interaction'
 import './Explore.css'
 import Map from '../Explore/Game/Map/Map'
 import Info from '../../Layout/Info/Info'
 
 function Explore() {
-    // make this component have an info button that displays the p tag below
-    return (
-        <>
-        <div>
-        <Info text={"Gem's are the secret to knowledge! Click around and learn about Recycling, earn points, and even get insights into tomorrow's question of the day!"}/>
-        </div>
-            {/* <MapInteractionCSS> */}
-            <Map/><br/><br/><br/><br/>
-            {/* </MapInteractionCSS> */}
+  const [gems, setGems] = useState([])
+  const [collectedCount, setCollectedCount] = useState(0)
+  const [collectedKnowledge, setCollectedKnowledge] = useState([])
 
-        </>
-    )
+  useEffect(() => {
+    fetch('/allknowledge')
+      .then(r => r.json())
+      .then(data => setGems(data))
+  }, [])
+
+  const incrementCollected = () => {
+    setCollectedCount(prevCollected => prevCollected + 1)
+  }
+
+  return (
+    <>
+      <div>
+      <div className="gem-counter">
+        Gems Collected: {collectedCount} / {gems.length}
+      </div>
+        <Info text={"Gem's are the secret to knowledge! Click around and learn about Recycling, earn points, and even get insights into tomorrow's question of the day!"}/>
+      </div>
+      {/* <MapInteractionCSS> */}
+      <Map gems={gems} collectedCount={collectedCount} incrementCollected={incrementCollected} collectedKnowledge={collectedKnowledge} setCollectedKnowledge={setCollectedKnowledge}/><br/><br/><br/><br/>
+      {/* </MapInteractionCSS> */}
+    </>
+  )
 }
 
 export default Explore
