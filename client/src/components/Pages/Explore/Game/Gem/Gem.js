cf1da492d0da0e0e4efeabc3e0a09b8e56e00698
Sofia Katzman <katzmansof@gmail.com>
2023-08-21 21:22:07 -0400

    update gems to be from backend data

--- client/src/components/Pages/Explore/Game/Gem/Gem.js
+++ client/src/components/Pages/Explore/Game/Gem/Gem.js
@@ -1,15 +1,15 @@
-import React, {useState} from 'react';
-import './Gem.css';
+import React, {useState} from 'react'
+import './Gem.css'
 
 function Gem({ gem, onClick, hidden }) {
   const handleClick = () => {
-    onClick(gem);
-  };
+    onClick(gem)
+  }
 
   return (
     <div
       className={`gem ${hidden ? 'hidden' : ''}`}
-      style={{ top: `${gem.top}%`, left: `${gem.left}%` }}
+      style={{ top: `${gem.y}%`, left: `${gem.x}%` }}
       onClick={handleClick}
     >   
         {/* div below gem to glow since gem is not a square object */}
@@ -17,7 +17,7 @@ function Gem({ gem, onClick, hidden }) {
         <img src="https://freepngimg.com/save/56561-gem-download-hq-png/554x361" className="gem-image" alt="gem" />
       
     </div>
-  );
+  )
 }
 
-export default Gem;
+export default Gem
