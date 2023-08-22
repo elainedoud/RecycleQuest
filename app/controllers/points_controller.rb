0ce79b726aa804193490a02fc13a81c033dedf67
Elaine <elainedoud@gmail.com>
2023-08-22 09:00:52 -0400

    Updating methods within Points Controller

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -15,12 +15,12 @@ class PointsController < ApplicationController
     #Will likely need to experiment with assign_points method to make sure
     #it's working as expected
 
-    #def addpoints
-      #  user = User.find_by(username: params[:username])
-     #   point = Point.create(:user_id: user.id, :points_type: points_type_params[:points_type], :points_count[points_count])
-     #   render json: point
-     #       end
-    #    end
+    def addpoints
+        user = User.find_by(id: params[:id])
+        point = user.points.create(point_params)
+        render json: point
+            end
+        end
 
   #  def addpointsbytype
    #     user = User.find_by(username: params[:username])
@@ -48,10 +48,16 @@ class PointsController < ApplicationController
     end
 
     def point_params
-        params.permit(:user_id, :points_type, :points_count, :date)
+        params.permit(:user_id, :points_type, points_type_params(:points_count), :date)
     end
 
-    def points_type_params
-        params.permit(:daily_bonus, :daily_questions, :location_redemption, :recycle_redemption)
+    def points_type_params(points_type)
+        case points_type
+        when :daily_bonus, :daily_questions, :location_redemption, :recycle_redemption
+            params.permit(:points_type)
+        end
     end
+
+
+   
 end
